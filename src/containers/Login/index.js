06a019f3d55012b69e6d23f108fd8cacc5a06d40
michellekankan <michellekan0102@gmail.com>
2024-06-25 16:03:31 -0700

    feat: 🎸 add eslint config

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,43 +1,49 @@
-import { Button, Form, Input, Dialog } from 'antd-mobile';
+import {
+  Button, Form, Input, Dialog,
+} from 'antd-mobile';
 import './index.css';
+
 const initialValues = {
   username: 'michelle',
-  password: '123'
+  password: '123',
 };
 
 const Login = () => {
-  const [form] = Form.useForm()
+  const [form] = Form.useForm();
   const onSubmit = () => {
-    const values = form.getFieldsValue()
+    const values = form.getFieldsValue();
     Dialog.alert({
       content: <pre>{JSON.stringify(values, null, 2)}</pre>,
-    })
-  }
+    });
+  };
 
   return (
     <div className="login">
-      <Form 
-      form={form}
-      layout='horizontal' mode='card' initialValues={initialValues}
-      footer={
-        <Button block color='primary' onClick={onSubmit} size='large'>
-          Login
-        </Button>
-      }>
-        <Form.Item label='Username' name='username'>
-          <Input placeholder='Enter username' clearable />
+      <Form
+        form={form}
+        layout="horizontal"
+        mode="card"
+        initialValues={initialValues}
+        footer={(
+          <Button block color="primary" onClick={onSubmit} size="large">
+            Login
+          </Button>
+      )}
+      >
+        <Form.Item label="Username" name="username">
+          <Input placeholder="Enter username" clearable />
         </Form.Item>
-        <Form.Item label='Password' name='password'>
-          <Input placeholder='Enter password' clearable type='password'/>
+        <Form.Item label="Password" name="password">
+          <Input placeholder="Enter password" clearable type="password" />
         </Form.Item>
       </Form>
     </div>
   );
-}
+};
 
 export default Login;
 
-/*ch3 
+/* ch3
 引用 import { useState } from'react';
 const [name, setName] = useState('');
 const [pwd, setPwd] = useState('');
@@ -47,7 +53,7 @@ const [pwd, setPwd] = useState('');
 ch4
 引用antd-mobile
  <div className="login">
-      <Form 
+      <Form
       form={form} 要寫這個才有辦法傳到後端
       layout='horizontal' mode='card' initialValues={initialValues}
       footer={
