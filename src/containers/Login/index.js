8ca705b7c5b99e55bffeae3e347be5cde7c59d34
michellekankan <michellekan0102@gmail.com>
2024-06-25 15:22:31 -0700

    feat: 🎸 add a login demo by antd-mobile

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,29 +1,36 @@
-import { useState } from'react';
+import { Button, Form, Input, Dialog } from 'antd-mobile';
 import './index.css';
-
+const initialValues = {
+  username: 'michelle',
+  password: '123'
+};
 
 const Login = () => {
-  const [name, setName] = useState('');
-  const [pwd, setPwd] = useState('');
-  const clickHandler = () => {
-    alert('Login successfully ' + name + ',' + pwd);
-  }
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  }
-
-  const onChangePwd = (e) => {
-    setPwd(e.target.value);
+  const [form] = Form.useForm()
+  const onSubmit = () => {
+    const values = form.getFieldsValue()
+    Dialog.alert({
+      content: <pre>{JSON.stringify(values, null, 2)}</pre>,
+    })
   }
 
   return (
     <div className="login">
-      {name}
-      {pwd}
-      <div>Username<input onChange={onChangeName}/></div>
-      <div>Password<input type="password" onChange={onChangePwd}/></div>
-      <div><button onClick={clickHandler}>Login</button></div>
+      <Form 
+      form={form}
+      layout='horizontal' mode='card' initialValues={initialValues}
+      footer={
+        <Button block color='primary' onClick={onSubmit} size='large'>
+          Login
+        </Button>
+      }>
+        <Form.Item label='Username' name='username'>
+          <Input placeholder='Enter username' clearable />
+        </Form.Item>
+        <Form.Item label='Password' name='password'>
+          <Input placeholder='Enter password' clearable type='password'/>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
@@ -36,5 +43,25 @@ const [name, setName] = useState('');
 const [pwd, setPwd] = useState('');
 {name}
 {pwd}
+
+ch4
+引用antd-mobile
+ <div className="login">
+      <Form 
+      form={form} 要寫這個才有辦法傳到後端
+      layout='horizontal' mode='card' initialValues={initialValues}
+      footer={
+        <Button block color='primary' onClick={onSubmit} size='large'>
+          Login
+        </Button>
+      }>
+        <Form.Item label='Username' name='username'>
+          <Input placeholder='Enter username' />
+        </Form.Item>
+        <Form.Item name='password'>
+          <PasscodeInput length={3} keyboard={<NumberKeyboard />}/>
+        </Form.Item>
+      </Form>
+    </div>
 
 */
